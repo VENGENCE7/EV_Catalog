e294fcd38521283c7cd609627e646a9acb49f3c3
Bhavish Anand <86911386+VENGENCE7@users.noreply.github.com>
2022-04-23 10:38:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import EV from "./Components/EV.js";
-// import UseReducer from "./Components/Hooks/useReducer.js";
-// import UseState from "./Components/Hooks/useState";
-// import UseEffect from "./Components/Hooks/useEffect";
 
 const App = () => {
   return (
